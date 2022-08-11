8ef1d05a3136089a1160b305fcc4e5f092fa952e
Luís Clício <cliciosa14@gmail.com>
2022-08-10 22:13:00 -0300

    build: initial Next.js setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  return res.status(200).json({ name: 'Projeto Adimó' });
 }
---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Grupo Cultural Adimó</title>
+        <meta
+          name="description"
+          content="Esporte e cultura uma questão de cidadania"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Projeto Adimó</h1>
+    </div>
+  );
+}
