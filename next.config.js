ef50892e7ba4d65e4934568d3f4977fd9c1eef3a
Luís Clício <cliciosa14@gmail.com>
2022-08-15 10:22:27 -0300

    build: defines public routes in Next Config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,35 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/sobre',
+        destination: '/about',
+      },
+      {
+        source: '/galeria',
+        destination: '/gallery',
+      },
+      {
+        source: '/eventos',
+        destination: '/events',
+      },
+      {
+        source: '/noticias',
+        destination: '/posts',
+      },
+      {
+        source: '/parceiros',
+        destination: '/collaborators',
+      },
+      {
+        source: '/apoie',
+        destination: '/sponsor',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
