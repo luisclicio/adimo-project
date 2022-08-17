0fe4ab17c1989749ab6206999bd23ca9d14058a2
Luís Clício <cliciosa14@gmail.com>
2022-08-16 23:41:06 -0300

    feat: creates copy button component

---
+++ components/CopyButton.jsx
@@ -0,0 +1,32 @@
+import {
+  CopyButton as CopyButtonMantine,
+  ActionIcon,
+  Tooltip,
+} from '@mantine/core';
+import { IconCopy, IconCheck } from '@tabler/icons';
+
+export function CopyButton({
+  value,
+  className,
+  icon = <IconCopy size={16} />,
+}) {
+  return (
+    <CopyButtonMantine value={value} timeout={2500}>
+      {({ copied, copy }) => (
+        <Tooltip
+          label={copied ? 'Copiado' : 'Copiar'}
+          withArrow
+          position="right"
+        >
+          <ActionIcon
+            color={copied ? 'teal' : 'gray'}
+            onClick={copy}
+            className={className}
+          >
+            {copied ? <IconCheck size={16} /> : icon}
+          </ActionIcon>
+        </Tooltip>
+      )}
+    </CopyButtonMantine>
+  );
+}
