5977e0a4142252bc3225af095125894d3cbde5f6
Luís Clício <cliciosa14@gmail.com>
2022-08-16 23:53:10 -0300

    test: creates fake service data

---
+++ services/fake.js
@@ -0,0 +1,142 @@
+const fakeData = {
+  carrouselImages: [
+    { caption: '', url: 'https://picsum.photos/id/243/1000/700' },
+    { caption: '', url: 'https://picsum.photos/id/238/1000/700' },
+    { caption: '', url: 'https://picsum.photos/id/239/1000/700' },
+    { caption: '', url: 'https://picsum.photos/id/240/1000/700' },
+  ],
+
+  activities: [
+    {
+      title: 'Oficina de dança',
+      schedule: 'Quartas-feiras, das 16h as 18h',
+      image: 'https://picsum.photos/id/1004/1000/700',
+    },
+    {
+      title: 'Esportes',
+      schedule: 'Quartas-feiras, das 16h as 18h',
+      image: 'https://picsum.photos/id/1011/1000/700',
+    },
+    {
+      title: 'Teatro',
+      schedule: 'Quartas-feiras, das 16h as 18h',
+      image: 'https://picsum.photos/id/104/1000/700',
+    },
+    {
+      title: 'Oficina de música',
+      schedule: 'Quartas-feiras, das 16h as 18h',
+      image: 'https://picsum.photos/id/1082/1000/700',
+    },
+  ],
+
+  testimonials: [
+    {
+      author: {
+        name: 'João Pedro',
+        role: 'Ex-aluno',
+        image: 'https://picsum.photos/id/1012/3973/2639',
+      },
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+    },
+    {
+      author: {
+        name: 'Amanda Costa',
+        role: 'Voluntária',
+        image: 'https://picsum.photos/id/177/3973/2639',
+      },
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+    },
+    {
+      author: {
+        name: 'Leonardo Gonçalves',
+        role: 'Parceiro',
+        image: 'https://picsum.photos/id/1/3973/2639',
+      },
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+    },
+    {
+      author: {
+        name: 'Denise Garcia',
+        role: 'Aluna',
+        image: 'https://picsum.photos/id/1014/3973/2639',
+      },
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+    },
+  ],
+
+  events: [
+    {
+      slug: 'lorem-ipsum',
+      title: 'Lorem ipsum dolor',
+      local: 'Lorem ipsum dolor, Lorem ipsum',
+      image: 'https://picsum.photos/id/238/1000/700',
+      date: '2022-08-16T10:34:10',
+    },
+    {
+      slug: 'lorem-event',
+      title: 'Lorem ipsum dolor',
+      local: 'Lorem ipsum dolor, Lorem ipsum',
+      image: 'https://picsum.photos/id/248/1000/700',
+      date: '2022-08-16T10:34:10',
+    },
+  ],
+
+  articles: [
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+      slug: 'lorem-ipsum',
+      image: 'https://picsum.photos/id/238/1000/700',
+      publishedAt: '2022-08-16T10:34:10',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+      slug: 'lorem-ipsum',
+      image: 'https://picsum.photos/id/248/1000/700',
+      publishedAt: '2022-08-16T10:34:10',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet consectetur',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur, eos quis earum, nulla vero recusandae deleniti quia ipsum aspernatur cupiditate mollitia!',
+      slug: 'lorem-ipsum',
+      image: 'https://picsum.photos/id/235/1000/700',
+      publishedAt: '2022-08-16T10:34:10',
+    },
+  ],
+
+  collaborators: [
+    {
+      name: 'João Pedro',
+      role: 'Voluntário',
+      avatar: 'https://picsum.photos/id/1012/3973/2639',
+      followLink: 'https://picsum.photos/id/1012/3973/2639',
+    },
+    {
+      name: 'Denise Garcia',
+      role: 'Apoiadora',
+      avatar: 'https://picsum.photos/id/1014/3973/2639',
+    },
+    {
+      name: 'Leonardo Gonçalves',
+      role: 'Padrinho',
+      avatar: 'https://picsum.photos/id/1/3973/2639',
+      followLink: 'https://picsum.photos/id/1/3973/2639',
+    },
+    {
+      name: 'Amanda Costa',
+      role: 'Apoiadora',
+      avatar: 'https://picsum.photos/id/177/3973/2639',
+      followLink: 'https://picsum.photos/id/177/3973/2639',
+    },
+  ],
+};
+
+export default fakeData;
