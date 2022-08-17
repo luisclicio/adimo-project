677222b082499ef7ef073b2d546905fc2896b7aa
Luís Clício <cliciosa14@gmail.com>
2022-08-16 23:50:26 -0300

    feat: creates collaborator card component

---
+++ components/CollaboratorCard.jsx
@@ -0,0 +1,37 @@
+import { Avatar, Text, Button, Paper } from '@mantine/core';
+
+export function CollaboratorCard({ avatar, name, role, followLink }) {
+  return (
+    <Paper
+      p="md"
+      sx={(theme) => ({
+        backgroundColor: theme.colors.dark[8],
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      })}
+    >
+      <Avatar src={avatar} size={120} radius={120} mx="auto" />
+      <Text align="center" size="lg" weight="bold" mt="md" mb={0}>
+        {name}
+      </Text>
+      <Text align="center" color="dimmed" my={0}>
+        {role}
+      </Text>
+
+      {followLink && (
+        <Button
+          component="a"
+          href={followLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          fullWidth
+          mt="md"
+        >
+          Seguir
+        </Button>
+      )}
+    </Paper>
+  );
+}
