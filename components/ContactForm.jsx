0ddec67fa87e539b9a3732cdb7f422c278ab5616
Luís Clício <cliciosa14@gmail.com>
2022-10-04 08:00:36 -0300

    feat: persist contact form data in the Local Storage

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -9,6 +9,7 @@ import {
   createStyles,
   Autocomplete,
 } from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
 
 const useStyles = createStyles((theme) => {
   const BREAKPOINT = theme.fn.smallerThan('sm');
@@ -50,12 +51,42 @@ export function ContactForm({
 }) {
   const { classes, cx } = useStyles();
 
+  const [name, setName] = useLocalStorage({
+    key: 'contact.name',
+    defaultValue: '',
+  });
+  const [email, setEmail] = useLocalStorage({
+    key: 'contact.email',
+    defaultValue: '',
+  });
+  const [subject, setSubject] = useLocalStorage({
+    key: 'contact.subject',
+    defaultValue: '',
+  });
+  const [message, setMessage] = useLocalStorage({
+    key: 'contact.message',
+    defaultValue: '',
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    clearForm();
+  }
+
+  function clearForm() {
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessage('');
+  }
+
   return (
     <Box
       component="form"
       className={cx(classes.form, className)}
       sx={sx}
-      onSubmit={(event) => event.preventDefault()}
+      onSubmit={handleSubmit}
     >
       <Text size="xl" className={classes.title}>
         Fale conosco
@@ -65,13 +96,21 @@ export function ContactForm({
         cols={elementsFullWidth ? 1 : 2}
         breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
       >
-        <TextInput label="Seu nome" placeholder="Ex: João Silva" required />
+        <TextInput
+          label="Seu nome"
+          placeholder="Ex: João Silva"
+          required
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
 
         <TextInput
           type="email"
           label="Seu e-mail"
           placeholder="Ex: joaosilva@mail.com"
           required
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </SimpleGrid>
 
@@ -89,6 +128,8 @@ export function ContactForm({
           'Outros',
         ]}
         required
+        value={subject}
+        onChange={setSubject}
       />
 
       <Textarea
@@ -97,6 +138,8 @@ export function ContactForm({
         placeholder="Conteúdo da mensagem"
         minRows={3}
         required
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
       />
 
       <Group position="right" mt="md">
