116edc2c37864c99df2f294068c64a4546cde0c0
Luís Clício <cliciosa14@gmail.com>
2022-10-15 17:30:01 -0300

    feat: loads gallery page data from Hygraph

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,15 +1,13 @@
 import { Header, Title, Text } from '@mantine/core';
+import { gql } from '@apollo/client';
 
 import { MainLayout } from '../components/layout/MainLayout';
 import { AppGrid } from '../components/AppGrid';
 import { PhotoCard } from '../components/PhotoCard';
 
-// Fake data
-import FAKE from '../services/fake';
+import { hygraph } from '../services/hygraph';
 
-export default function Gallery() {
-  const photos = FAKE.photos;
-
+export default function Gallery({ photos = [] }) {
   return (
     <MainLayout title="Galeria de fotos">
       <Header unstyled py="xl">
@@ -18,14 +16,41 @@ export default function Gallery() {
       </Header>
 
       <AppGrid>
-        {photos.map((photo) => (
-          <PhotoCard
-            key={photo.image}
-            image={photo.image}
-            caption={photo.caption}
-          />
-        ))}
+        {photos.length > 0 ? (
+          photos.map((photo) => (
+            <PhotoCard
+              key={photo.id}
+              image={photo.image.url}
+              caption={photo.caption}
+            />
+          ))
+        ) : (
+          <Text>Nenhuma foto publicada.</Text>
+        )}
       </AppGrid>
     </MainLayout>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await hygraph.query({
+    query: gql`
+      query GetPhotos {
+        photos(orderBy: publishedAt_DESC) {
+          id
+          caption
+          image {
+            url
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      photos: data?.photos ?? [],
+    },
+    revalidate: 60, // Revalidate page after 60 seconds
+  };
 }
