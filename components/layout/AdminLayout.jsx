70e96aedc6d130805aa74f4547b2b3b683320c97
Luís Clício <cliciosa14@gmail.com>
2022-10-31 21:15:32 -0300

    feat: protects admin panel

--- components/layout/AdminLayout.jsx
+++ components/layout/AdminLayout.jsx
@@ -1,9 +1,23 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 import { Container } from '@mantine/core';
 
 import { AdminNavbar } from '../navbar/AdminNavbar';
 
 export function AdminLayout({ title, children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace('/login');
+    },
+  });
+
+  if (status === 'loading') {
+    return;
+  }
+
   return (
     <>
       <Head>
